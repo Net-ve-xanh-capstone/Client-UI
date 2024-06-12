a9d5a31d9d87ea86739579f76888509c47763539
DinhB <dinhbach3062001@gmail.com>
2024-06-12 22:45:10 +0700

    add authen

--- src/api/authenApi.js
+++ src/api/authenApi.js
@@ -1,7 +1,18 @@
 import axiosApi from "./axiosApi";
+import tokenService from "./service/tokenService";
 
 export const auhtenApi = {
   login: async (endpoint, data, config) => {
     return await axiosApi.post(endpoint, data, config);
+  },
+  refreshToken: async () => {
+    const refreshToken = tokenService.getLocalRefreshToken();
+    try {
+      const response = await axios.post(`${BASE_URL}/refresh-token`,{ refreshToken });                      
+      return response.data;
+    } catch (error) {
+        console.error('Error refreshing token:', error);
+        throw error;
+    }
   },
 };
--- src/api/axiosApi.js
+++ src/api/axiosApi.js
@@ -1,37 +1,78 @@
 import axios from "axios";
+import { auhtenApi } from "./authenApi";
+import tokenService from "./service/tokenService";
 
 const baseUrl = "http://localhost:3001";
+
+let isRefreshing = false;
+let failedQueue = [];
 
 const axiosApi = axios.create({
   baseURL: baseUrl,
   Headers: { "Content-Type": "application/json" },
 });
 
-// Add a request interceptor
-// axios.interceptors.request.use(úh
-//   (config) => {
-//     const token = localStorage.getItem("localtoken");
-//     if (token) {
-//       config.headers.Authorization = `Bearer ${token}`;
-//     }
-//     return config;
-//   },
-//   (error) => {
-//     return Promise.reject(error);
-//   }
-// );
+// Add the auth token to every request
+axiosApi.interceptors.request.use(
+  config => {
+      const token = tokenService.getLocalAccessToken();
+      if (token) {
+          config.headers['Authorization'] = `Bearer ${token}`;
+      }
+      return config;
+  },
+  error => Promise.reject(error)
+);
+
+function subscribeTokenRefresh(cb) {
+  failedQueue.push(cb);
+}
+
+function onRefreshed(token) {
+  failedQueue.forEach(cb => cb(token));
+  failedQueue = [];
+}
 
 // Add a response interceptor
-axios.interceptors.response.use(
-  function (response) {
-    // Any status code that lie within the range of 2xx cause this function to trigger
-    // Do something with response data
-    return response.data;
-  },
-  function (error) {
-    // Any status codes that falls outside the range of 2xx cause this function to trigger
-    // Do something with response error
-    return Promise.reject(error);
+axiosApi.interceptors.response.use(
+  response => response,
+  err => {
+      const { config, response } = err;
+      const originalRequest = config;
+      if (response && response.status === 401 && !originalRequest._retry) {
+          if (!isRefreshing) {
+              isRefreshing = true;
+              return auhtenApi.refreshToken()
+                  .then(({ access_token, refresh_token }) => {
+                   tokenService.updateLocalAccessToken(access_token);
+                    tokenService.updateLocalRefreshToken(refresh_token);
+                     request.defaults.headers.common['Authorization'] = `Bearer${access_token}`;        
+                      isRefreshing = false;
+                      onRefreshed(access_token);
+return Promise.all(failedQueue.map(cb =>   cb(access_token))).then(() => {
+                          return axios(originalRequest);
+                      });
+                  })
+                  .catch(error => {
+                      isRefreshing = false;
+                      tokenService.clearTokens();
+                      navigationRef.navigate('Login');
+                      return Promise.reject(error);
+                  })
+                  .finally(() => failedQueue = []);
+          }
+
+          originalRequest._retry = true;
+          return new Promise((resolve, reject) => {
+              subscribeTokenRefresh(token => {
+                  originalRequest.headers['Authorization'] = `Bearer ${token}`;
+                  resolve(axios(originalRequest));
+              });
+          });
+      }
+
+      return Promise.reject(err);
   }
 );
+
 export default axiosApi;

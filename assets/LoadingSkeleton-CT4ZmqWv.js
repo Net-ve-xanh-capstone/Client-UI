2e264fadb2415f2301b91057cbb054fed67ecd83
dinhbach306 <dinhbach306@users.noreply.github.com>
2024-07-28 18:32:44 +0000

    deploy: 6f5d2cd3ed317503cb63b2757512008ab006fda5

---
+++ assets/LoadingSkeleton-CT4ZmqWv.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-0h_SDAPj.js";const s=t=>e.jsx("div",{className:"skeleton",style:{height:t.height,width:t.width||"100%",borderRadius:t.radius}});export{s as L};
--- assets/LoadingSkeleton-Cg0e_na6.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-WK5Y_eAK.js";const s=t=>e.jsx("div",{className:"skeleton",style:{height:t.height,width:t.width||"100%",borderRadius:t.radius}});export{s as L};

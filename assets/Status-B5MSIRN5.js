992d170e9c2cc960fd9717b6e0822ac3977dd41e
dinhbach306 <dinhbach306@users.noreply.github.com>
2024-08-03 20:54:38 +0000

    deploy: a5aac4368bba4dcefd996c7c23a89829628e710f

---
+++ assets/Status-B5MSIRN5.js
@@ -0,0 +1,1 @@
+const t={NOT_STARTED:"Đã hoàn thành",IN_PROCESS:"Đang tiến hành"},a={SUBMITTED:"Submitted",DRAFT:"Draft",ACCEPTED:"Accepted"},n=["Submitted","Accepted"],s=["Draft","New"];export{a,s as b,t as c,n as p};
--- assets/Status-Nhevn1i3.js
+++
@@ -1,1 +0,0 @@
-const t={NOT_STARTED:"NotStarted",IN_PROCESS:"InProcess"},a={SUBMITTED:"Submitted",DRAFT:"Draft",ACCEPTED:"Accepted"},s=["Submitted","Accepted"],e=["Draft","New"];export{a,e as b,t as c,s as p};

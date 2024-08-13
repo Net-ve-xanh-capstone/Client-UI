41b9b5077829c8d165e0afb45264fff1ae052972
quyendtt <quyendtt.contact@gmail.com>
2024-08-13 14:02:12 +0700

    ...

--- src/components/addPainting/page.jsx
+++ src/components/addPainting/page.jsx
@@ -479,7 +479,9 @@ function ModalAddPainting({ modalShow, onHide, fetchData, setPageNumber }) {
 
   // after click on the round selected then will setit to final
   const fetchRoundTopic = async (id, label) => {
-    finalCondition(label.split(' -')[0] === 'Chung Kết', id);
+    console.log(label.split(' -')[0] === 'Vòng Chung Kết');
+
+    finalCondition(label.split(' -')[0] === 'Vòng Chung Kết', id);
     setLoadingRound(true);
     try {
       const res = await roundTopicById(id);

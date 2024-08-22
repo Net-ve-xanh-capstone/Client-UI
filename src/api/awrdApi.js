cf8ee176a1d88d7957984be4963fa545e2103bd0
quyendtt <quyendtt.contact@gmail.com>
2024-08-22 12:18:58 +0700

    adding new fearture to award management

--- src/api/awrdApi.js
+++ src/api/awrdApi.js
@@ -3,3 +3,15 @@ import axiosApi from './axiosApi';
 export const getAwardId = id => {
   return axiosApi.get(`awards/Round/${id}`);
 };
+
+export const putAwards = payload => {
+  return axiosApi.put('awards', payload);
+};
+
+export const createAward = payload => {
+  return axiosApi.post('awards', payload);
+};
+
+export const deleteAward = id => {
+  return axiosApi.patch(`awards?id=${id}`);
+};

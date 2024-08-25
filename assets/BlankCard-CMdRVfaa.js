29b60429d254e8e4e23906505f042344758d4b81
dinhbach306 <dinhbach306@users.noreply.github.com>
2024-08-25 07:09:35 +0000

    deploy: 3d9c340e68cf66a9848daf4c8dcf1a547a5b1b20

--- assets/BlankCard-CMdRVfaa.js
+++
@@ -1,1 +0,0 @@
-import{P as a,j as s}from"./index-_RrZMV6M.js";import{a as e}from"./DashboardCard-0D_bKLQb.js";const p=({children:r,className:o})=>s.jsx(e,{sx:{p:0,position:"relative"},className:o,elevation:9,variant:void 0,children:r});p.propTypes={children:a.node};export{p as B};

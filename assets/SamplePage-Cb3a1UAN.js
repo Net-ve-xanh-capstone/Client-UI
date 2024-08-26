1a25d7ba5659dbeef42581db927b0a1987db9281
dinhbach306 <dinhbach306@users.noreply.github.com>
2024-08-26 04:15:34 +0000

    deploy: dfe6401c11dca5bee70a07f1acb225093af6d5dc

--- assets/SamplePage-Cb3a1UAN.js
+++
@@ -1,1 +0,0 @@
-import{j as a,af as e}from"./index-NIIUBiO7.js";import{P as s,D as r}from"./DashboardCard-DB3nPh7J.js";import"./Box-DNwrR1g1.js";const o=()=>a.jsx(s,{title:"Sample Page",description:"this is Sample page",children:a.jsx(r,{title:"Sample Page",children:a.jsx(e,{children:"This is a sample page"})})});export{o as default};

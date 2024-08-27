b50c97592851f9871f173d2263efbdf9f32cc1be
quyendtt <quyendtt@users.noreply.github.com>
2024-08-27 18:56:34 +0000

    deploy: 24d68227596f6ff90b8733a124164cdec246a27c

--- assets/BlankCard-Byaa7NA4.js
+++
@@ -1,1 +0,0 @@
-import{P as a,j as s}from"./index-DK9C9aCW.js";import{a as e}from"./DashboardCard-_GtGK-g8.js";const p=({children:r,className:o})=>s.jsx(e,{sx:{p:0,position:"relative"},className:o,elevation:9,variant:void 0,children:r});p.propTypes={children:a.node};export{p as B};
